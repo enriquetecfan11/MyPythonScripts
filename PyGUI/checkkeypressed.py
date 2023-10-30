fa9ec254731ce68b07ecfd640b737e6829b6bd2f
enriquetecfan11 <enriquemondejar15@gmail.com>
2023-10-30 16:19:26 +0100

    new script

--- PyGUI/checkkeypressed.py
+++ PyGUI/checkkeypressed.py
@@ -1,6 +1,8 @@
-import pyautogui
 import keyboard
 
-print('Press Ctrl-C to quit.')
+def on_key_event(event):
+    print(f"Key {event.name} {'pressed' if event.event_type == 'down' else 'released'}")
 
-# Check what keys are pressed
+keyboard.hook(on_key_event)
+
+keyboard.wait('esc')  # You can change the key to wait for a different key press
