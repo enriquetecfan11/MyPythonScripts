d36113b801c71ef204a1257f7e32d2813f7f1df3
enriquetecfan11 <enriquemondejar15@gmail.com>
2023-11-07 11:20:35 +0100

    Better README with a script

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
-# My Python Scripts
 
-Estos son mis archivos de python que uso cada dia
+# Welcome to Python Scripts
+This are my best scripts i wrote in python for my daily work.
+
+
+## Repository Structure
+- .git
+- CC
+- ChromeDriver
+- cursodjango
+- DB
+- Docker
+- MediaFiles
+- MySQL
+- Noticias
+- paginawebautomatizada
+- ProgramasVarios
+- Puentes
+- PyGame
+- PyGUI
+- RaspberryPi
+- SAM
+- ScriptsVarios
+- streamlit
+- TelegramBot
+- TestBots
+- TOM
+
+## License
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+    
+    Copyright 2023 Enrique Rodriguez Vela
--- test.py
+++ test.py
@@ -3,18 +3,44 @@ Created on Tue Non 7
 Autor:     Enrique Rodriguez
 """
 
-# Define two numbers
-num1 = 10
-num2 = 5
+import os
 
-# Perform basic arithmetic operations
-addition = num1 + num2
-subtraction = num1 - num2
-multiplication = num1 * num2
-division = num1 / num2
+def generate_readme(path):
+    readme_text = """
+    # Welcome to Python Scripts
 
-# Print the results
-print(f"Addition: {addition}")
-print(f"Subtraction: {subtraction}")
-print(f"Multiplication: {multiplication}")
-print(f"Division: {division}")
+    This are my best scripts i wrote in python for my daily work.
+
+    ## License
+    Copyright 2017 ChenQi
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+    
+    ## Repository Structure
+    """
+    
+    for root, dirs, _ in os.walk(path):
+        level = root.replace(path, '').count(os.sep)
+        if level == 1:  # Only process top-level directories
+            indent = ' ' * 4 * (level - 1)
+            readme_text += '{}- {}\n'.format(indent, os.path.basename(root))
+    return readme_text
+
+# Get the directory of the current script
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+readme_text = generate_readme(current_dir)
+
+# Now you can write the generated text to your README file
+with open('README.md', 'w') as f:
+    f.write(readme_text)
