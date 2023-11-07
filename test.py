bb977326bafe985df22d992de1f7837b90e03742
enriquetecfan11 <enriquemondejar15@gmail.com>
2023-11-07 11:01:16 +0100

    testing commit

---
+++ test.py
@@ -0,0 +1,20 @@
+"""
+Created on Tue Non 7
+Autor:     Enrique Rodriguez
+"""
+
+# Define two numbers
+num1 = 10
+num2 = 5
+
+# Perform basic arithmetic operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Print the results
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
