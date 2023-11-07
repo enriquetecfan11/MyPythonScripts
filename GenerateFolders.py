6b5c3da4bfcb32327bbd9a6ea53445ca22f5dd43
enriquetecfan11 <enriquemondejar15@gmail.com>
2023-11-07 16:13:29 +0100

    new script name and folders

---
+++ GenerateFolders.py
@@ -0,0 +1,62 @@
+"""
+This script generates a README.md file for a repository by listing the top-level directories and adding a license and contact section.
+
+Created on Tue Non 7
+Autor:     Enrique Rodriguez
+"""
+
+import os
+
+# Get the directory of the current script
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+readme_folders = """
+## Repository Structure
+"""
+  
+for root, dirs, _ in os.walk(current_dir):
+  level = root.replace(current_dir, '').count(os.sep)
+  if level == 1:  # Only process top-level directories
+    indent = ' ' * 4 * (level - 1)
+    readme_folders += '{}- {}\n'.format(indent, os.path.basename(root))
+
+
+readme_license = """
+## License
+Copyright 2023 Enrique Rodriguez
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+readme_welcome = """
+
+# Welcome to Python Scripts
+
+This are my best scripts i wrote in python for my daily work.
+
+"""
+
+readme_footer = """
+## Contact
+- [LinkedIn](https://www.linkedin.com/in/enrique-rodriguez-vela/)
+- [Twitter](https://twitter.com/kikerodrivela)
+- [GitHub](https://github.com/enriquetecfan11)
+"""
+
+readme_text = readme_welcome + readme_folders + readme_license + readme_footer
+
+# Now you can write the generated text to your README file
+with open('README.md', 'w') as f:
+  f.write(readme_text)
+
+print('README.md file generated')
--- README.md
+++ README.md
@@ -1,25 +1,12 @@
 
-    # Welcome to Python Scripts
 
-    This are my best scripts i wrote in python for my daily work.
+# Welcome to Python Scripts
 
-    ## License
-    Copyright 2017 ChenQi
+This are my best scripts i wrote in python for my daily work.
 
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-    
-    ## Repository Structure
-    - .git
+## Repository Structure
+- .git
 - ChromeDriver
 - cursodjango
 - Database
@@ -40,3 +27,23 @@
 - streamlit
 - TelegramBot
 - TestBots
+
+## License
+Copyright 2023 Enrique Rodriguez
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+## Contact
+- [LinkedIn](https://www.linkedin.com/in/enrique-rodriguez-vela/)
+- [Twitter](https://twitter.com/kikerodrivela)
+- [GitHub](https://github.com/enriquetecfan11)
