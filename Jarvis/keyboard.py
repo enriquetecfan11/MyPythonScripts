05a61df04defb4b0fbf33e6b587a30be6f282cff
Enrique Rodriguez Vela <enriquemondejar15@gmail.com>
2024-08-27 09:17:31 +0200

    chore: Remove conflicting code and resolve merge conflicts

--- Jarvis/keyboard.py
+++ Jarvis/keyboard.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from pynput import keyboard
 
 def on_press(key):
@@ -13,19 +12,3 @@ listener.start()
 # Mantén el programa en ejecución hasta que el usuario lo detenga
 input()
 listener.stop()
-=======
-from pynput import keyboard
-
-def on_press(key):
-    try:
-        print('Letra presionada: {0}'.format(key.char))
-    except AttributeError:
-        print('Tecla especial presionada: {0}'.format(key))
-
-listener = keyboard.Listener(on_press=on_press)
-listener.start()
-
-# Mantén el programa en ejecución hasta que el usuario lo detenga
-input()
-listener.stop()
->>>>>>> 23417d7b6ed142e1bebf4f7fa64b340fe4c64c5b
